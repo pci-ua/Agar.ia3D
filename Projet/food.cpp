7f80cf3dcf3f62406a10fba1ae505e11344652c6
StrategFirst <mathieu.toulon@free.fr>
2021-05-26 17:38:23 +0200

    Retrait ancien dossier projet

--- Projet/food.cpp
+++
@@ -1,7 +0,0 @@
-#include "food.h"
-
-// change la position de la food sur le terrain
-void Food::SeFaireManger() {
-  this->_posX=(float)rand()/(RAND_MAX)*(LONGUEUR-MARGE+LONGUEUR-MARGE)-LONGUEUR+MARGE;
-  this->_posZ=(float)rand()/(RAND_MAX)*(LARGEUR-MARGE+LARGEUR-MARGE)-LARGEUR+MARGE;
-}
