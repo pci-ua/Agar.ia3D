e14a738c3fa36a13cd5f7903d60b38535a6bb65b
StrategFirst <mathieu.toulon@free.fr>
2021-06-10 00:59:54 +0200

    Factorisation déplacement

--- jeu_complet/ia.cpp
+++ jeu_complet/ia.cpp
@@ -5,62 +5,30 @@ extern Ia iatest[];
 // déplacement de l'ia à la position i dans le tableau ia
 void Ia::deplacement (int width,int height){
   // stock un nombre aléatoire entre 0 et la taille de la fenêtre
-  int x2=rand()%width;
-  int y2=rand()%height;
+  int x2= (rand()%width) - width/2;
+  int y2= (rand()%height) - height/2;
   // stocke les positions aléatoires dans des variables de la classe ia (_posAleatoireX,_posAleatoireY)
   this->setiaX(x2);
   this->setiaY(y2);
-  float coeffdirecteur=0.0;
-  // vérification pour ne pas avoir de division par 0 et calcul du coefficient directeur
-  if(_posAleatoireX!=width/2){
-      coeffdirecteur= (static_cast<float>(_posAleatoireY)-static_cast<float>(height/2))/(static_cast<float>(_posAleatoireX)-static_cast<float>(width/2));
-  }
-  // récupère l'angle en radian par rapport à l'axe des x et la position du "curseur"(non visible mais imaginé par _posAleatoireX,_posAleatoireY) de l'ia
-  float angle=atan(coeffdirecteur);
-  // l'angle est supérieur à 0 lorsque le "curseur" se trouve en haut à gauche ou en bas à droite
-  if(angle>0){
-    // si le "curseur" se trouve dans la partie gauche de l'écran (donc en haut à gauche) et qu'il ne va pas sortir du terrain si l'on va dans cette direction
-    if((width/2>_posAleatoireX) && (this->getX()>-LONGUEUR+this->_taille) && (this->getZ()>-LARGEUR+this->_taille)) {
-      // change la position dans la direction du "curseur" avec la vitesse de l'ia
-      this->setX(this->getX()-this->_vitesse*(cos(angle)));
-      this->setZ(this->getZ()-this->_vitesse*(sin(angle)));
-    }
-    // si le "curseur" se trouve dans la partie droite de l'écran (donc en bas à droite) et qu'il ne va pas sortir du terrain si l'on va dans cette direction
-    else if((width/2<_posAleatoireX) && (this->getX()<LONGUEUR-this->_taille) && (this->getZ()<LARGEUR-this->_taille)){
-      this->setX(this->getX()+this->_vitesse*(cos(angle)));
-      this->setZ(this->getZ()+this->_vitesse*(sin(angle)));
-    }
-  }
-  // l'angle est inférieur à 0 lorsque le "curseur" se trouve en haut à droite ou en bas à gauche
-  if(angle<0){
-    // si le "curseur" se trouve dans la partie gauche de l'écran (donc en bas à gauche) et qu'il ne va pas sortir du terrain si l'on va dans cette direction
-    if((width/2>_posAleatoireX) && (this->getX()>-LONGUEUR+this->_taille) && (this->getZ()<LARGEUR-this->_taille)) {
-      this->setX(this->getX()-this->_vitesse*(cos(angle)));
-      this->setZ(this->getZ()-this->_vitesse*(sin(angle)));
-    }
-    // si le "curseur" se trouve dans la partie droite de l'écran (donc en haut à droite) et qu'il ne va pas sortir du terrain si l'on va dans cette direction
-    else if((width/2<_posAleatoireX) && (this->getX()<LONGUEUR-this->_taille) && (this->getZ()>-LARGEUR+this->_taille)){
-      this->setX(this->getX()+this->_vitesse*(cos(angle)));
-      this->setZ(this->getZ()+this->_vitesse*(sin(angle)));
-    }
-  }
-  // lorsque la souris est dans l'axe vertical ou horizontal
-  if(angle==0){
-    // si le "curseur" se trouve dans l'axe horizontal à gauche et qu'il ne va pas sortir du terrain si l'on va dans cette direction
-    if((width/2>_posAleatoireX) && (this->getX()>-LONGUEUR+this->_taille)){
-      this->setX(this->getX()-this->_vitesse*(cos(angle)));
-    }
-    // si le "curseur" se trouve dans l'axe horizontal à droite et qu'il ne va pas sortir du terrain si l'on va dans cette direction
-    else if((width/2<_posAleatoireX) && (this->getX()<LONGUEUR-this->_taille)){
-      this->setX(this->getX()+this->_vitesse*(cos(angle)));
-    }
-    // si le "curseur" se trouve dans l'axe vertical en haut et qu'il ne va pas sortir du terrain si l'on va dans cette direction
-    else if((height/2>_posAleatoireY) && (this->getZ()>-LARGEUR+this->_taille)){
-      this->setZ(this->getZ()-this->_vitesse*(cos(angle)));
-    }
-    // si le "curseur" se trouve dans l'axe vertical en bas et qu'il ne va pas sortir du terrain si l'on va dans cette direction
-    else if((height/2<_posAleatoireY) && (this->getZ()<LARGEUR-this->_taille)){
-      this->setZ(this->getZ()+this->_vitesse*(cos(angle)));
+
+  float Magnitude = sqrt( pow( _posAleatoireX , 2 ) + pow( _posAleatoireY , 2 ) );
+  if( Magnitude != 0 ) {
+    //Vecteur unitaire de déplacement
+    float vX = _vitesse * _posAleatoireX / Magnitude;
+    float vZ = _vitesse * _posAleatoireY / Magnitude;
+
+    //position à la prochaine frame
+    float pX = this->getX() + vX;
+    float pZ = this->getZ() + vZ;
+
+    //si reste dans la bordure on bouge
+    if(
+      -LARGEUR + _taille <= pX && pX <= LARGEUR - _taille
+      &&
+      -LONGUEUR + _taille <= pZ && pZ <= LONGUEUR - _taille
+    ) {
+      this->setX( pX );
+      this->setZ( pZ );
     }
   }
 }
