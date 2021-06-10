a1bd5cdf38049d5e9ff4175e340a08177d13c73a
StrategFirst <mathieu.toulon@free.fr>
2021-06-10 19:54:46 +0200

    Correction indentation

--- jeu_complet/boule.hh
+++ jeu_complet/boule.hh
@@ -3,29 +3,30 @@
 #include "constante.hh"
 
 class Boule{
-  public:
-    Boule(float taille);
-    ~Boule() = default;
-    Boule(const Boule & c) = default;
 
-    void draw() const;
+public:
+	Boule(float taille);
+	~Boule() = default;
+	Boule(const Boule & c) = default;
 
-    float getX() const{ return _posX;}
-    float getZ() const{ return _posZ;}
-    float getTaille() const{return _taille;}
+	void draw() const;
 
-    void setX(float x){this->_posX=x;}
-    void setZ(float z){this->_posZ=z;}
-    void setr(float r){this->_r=r;}
-    void setg(float g){this->_g=g;}
-    void setb(float b){this->_b=b;}
+	float getX() const{ return _posX;}
+	float getZ() const{ return _posZ;}
+	float getTaille() const{return _taille;}
 
-  protected:
-    float _posX;
-    float _posZ;
-    float _taille;
+	void setX(float x){this->_posX=x;}
+	void setZ(float z){this->_posZ=z;}
+	void setr(float r){this->_r=r;}
+	void setg(float g){this->_g=g;}
+	void setb(float b){this->_b=b;}
 
-    float _r;
-    float _g;
-    float _b;
+protected:
+	float _posX;
+	float _posZ;
+	float _taille;
+
+	float _r;
+	float _g;
+	float _b;
 };
