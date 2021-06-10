caba56e8f7bd3bb0bd4789a7ea8dfa03768615d1
StrategFirst <mathieu.toulon@free.fr>
2021-06-10 21:02:13 +0200

    Généré les couleurs depuis une seed différnete à chaque itération

--- jeu_complet/main.cpp
+++ jeu_complet/main.cpp
@@ -1,10 +1,12 @@
 #include "mainwindow.hh"
 #include <QApplication>
 #include <GL/glut.h>
-
+#include <time.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
+     srand(time(nullptr));
     glutInit(&argc,argv);
 
     QApplication a(argc, argv);
