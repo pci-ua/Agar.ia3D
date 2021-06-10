5c9d47c2ff56cd0eed4fbd62d2f7bc400075c2af
StrategFirst <mathieu.toulon@free.fr>
2021-06-10 19:42:33 +0200

    Retrait code inutilisé

--- jeu_complet/terrain.cpp
+++ jeu_complet/terrain.cpp
@@ -13,41 +13,5 @@ terrain::terrain(float longueur, float largeur)
   glTexCoord2f(0.0,6.0);
   glVertex3f(longueur,0.0,-largeur);
   glEnd();
-
-  /*
-  // mur avant
-  glColor3f(1.0,0.0,0.0);
-  glBegin(GL_QUADS);
-  glVertex3f(10.0,0.0,10.0);
-  glVertex3f(-10.0,0.0,10.0);
-  glVertex3f(-10.0,2.5,10.0);
-  glVertex3f(10.0,2.5,10.0);
-  glEnd();
-
-  //mur arrière
-  glColor3f(1.0,0.0,0.0);
-  glBegin(GL_QUADS);
-  glVertex3f(10.0,0.0,-10.0);
-  glVertex3f(-10.0,0.0,-10.0);
-  glVertex3f(-10.0,2.5,-10.0);
-  glVertex3f(10.0,2.5,-10.0);
-  glEnd();
-
-  //mur droit
-  glColor3f(1.0,0.0,0.0);
-  glBegin(GL_QUADS);
-  glVertex3f(10.0,0.0,-10.0);
-  glVertex3f(10.0,0.0,10.0);
-  glVertex3f(10.0,2.5,10.0);
-  glVertex3f(10.0,2.5,-10.0);
-  glEnd();
-
-  //mur gauche
-  glColor3f(1.0,0.0,0.0);
-  glBegin(GL_QUADS);
-  glVertex3f(-10.0,0.0,-10.0);
-  glVertex3f(-10.0,0.0,10.0);
-  glVertex3f(-10.0,2.5,10.0);
-  glVertex3f(-10.0,2.5,-10.0);
-  glEnd();*/
+  
 }
