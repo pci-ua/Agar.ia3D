8cc329782994a11df96cf9900727e2d1c78c0b91
StrategFirst <mathieu.toulon@free.fr>
2021-06-10 03:31:59 +0200

    Simplification code déplacement joueur

--- jeu_complet/joueur.cpp
+++ jeu_complet/joueur.cpp
@@ -36,70 +36,29 @@ void souris (int x, int y){
 
 
 void Joueur::deplacement (int width,int height){
-  float coeffdirecteur=0.0;
-  // vérification pour ne pas avoir de division par 0 et calcul du coefficient directeur
-  if(_posSourisX!=width/2){
-    coeffdirecteur= (static_cast<float>(_posSourisY)-static_cast<float>(height/2))/(static_cast<float>(_posSourisX)-static_cast<float>(width/2));
-  }
-  // récupère l'angle en radian par rapport à l'axe des x et la position du curseur de la souris (stocké dans _posSourisX,_posSourisY grâ à la methode souris)
-  float angle=atan(coeffdirecteur);
-  //printf("angle: %f \n", angle);
-  // l'angle est supérieur à 0 lorsque le curseur se trouve en haut à gauche ou en bas à droite
-  if(angle>0){
-    // si le curseur se trouve dans la partie gauche de l'écran (donc en haut à gauche) et qu'il ne va pas sortir du terrain si l'on va dans cette direction
-    if((width/2>_posSourisX) && (player.getX()>-LONGUEUR+this->_taille) && (player.getZ()>-LARGEUR+this->_taille)) {
-      // change la position dans la direction du curseur avec la vitesse de l'ia et fait suivre la caméra
-      player.setX(player.getX()-this->_vitesse*(cos(angle)));
-      x_cam=x_cam-this->_vitesse*(cos(angle));
-      player.setZ(player.getZ()-this->_vitesse*(sin(angle)));
-      z_cam=z_cam-this->_vitesse*(sin(angle));
-    }
-    // si le curseur se trouve dans la partie droite de l'écran (donc en bas à droite) et qu'il ne va pas sortir du terrain si l'on va dans cette direction
-    else if((width/2<_posSourisX) && (player.getX()<LONGUEUR-this->_taille) && (player.getZ()<LARGEUR-this->_taille)){
-      player.setX(player.getX()+this->_vitesse*(cos(angle)));
-      x_cam=x_cam+this->_vitesse*(cos(angle));
-      player.setZ(player.getZ()+this->_vitesse*(sin(angle)));
-      z_cam=z_cam+this->_vitesse*(sin(angle));
-    }
-  }
-  // l'angle est inférieur à 0 lorsque le curseur se trouve en haut à droite ou en bas à gauche
-  if(angle<0){
-    // si le curseur se trouve dans la partie gauche de l'écran (donc en bas à gauche) et qu'il ne va pas sortir du terrain si l'on va dans cette direction
-    if((width/2>_posSourisX) && (player.getX()>-LONGUEUR+this->_taille) && (player.getZ()<LARGEUR-this->_taille)) {
-      player.setX(player.getX()-this->_vitesse*(cos(angle)));
-      x_cam=x_cam-this->_vitesse*(cos(angle));
-      player.setZ(player.getZ()-this->_vitesse*(sin(angle)));
-      z_cam=z_cam-this->_vitesse*(sin(angle));
-    }
-    // si le curseur se trouve dans la partie droite de l'écran (donc en haut à droite) et qu'il ne va pas sortir du terrain si l'on va dans cette direction
-    else if((width/2<_posSourisX) && (player.getX()<LONGUEUR-this->_taille) && (player.getZ()>-LARGEUR+this->_taille)){
-      player.setX(player.getX()+this->_vitesse*(cos(angle)));
-      x_cam=x_cam+this->_vitesse*(cos(angle));
-      player.setZ(player.getZ()+this->_vitesse*(sin(angle)));
-      z_cam=z_cam+this->_vitesse*(sin(angle));
-    }
-  }
-  // lorsque la souris est dans l'axe vertical ou horizontal
-  if(angle==0){
-    // si le curseur se trouve dans l'axe horizontal à gauche et qu'il ne va pas sortir du terrain si l'on va dans cette direction
-    if((width/2>_posSourisX) && (player.getX()>-LONGUEUR+this->_taille)){
-      player.setX(player.getX()-this->_vitesse*(cos(angle)));
-      x_cam=x_cam-this->_vitesse*(cos(angle));
-    }
-    // si le curseur se trouve dans l'axe horizontal à droite et qu'il ne va pas sortir du terrain si l'on va dans cette direction
-    else if((width/2<_posSourisX) && (player.getX()<LONGUEUR-this->_taille)){
-      player.setX(player.getX()+this->_vitesse*(cos(angle)));
-      x_cam=x_cam+this->_vitesse*(cos(angle));
-    }
-    // si le curseur se trouve dans l'axe vertical en haut et qu'il ne va pas sortir du terrain si l'on va dans cette direction
-    else if((height/2>_posSourisY) && (player.getZ()>-LARGEUR+this->_taille)){
-      player.setZ(player.getZ()-this->_vitesse*(cos(angle)));
-      z_cam=z_cam-this->_vitesse*(cos(angle));
-    }
-    // si le curseur se trouve dans l'axe vertical en bas et qu'il ne va pas sortir du terrain si l'on va dans cette direction
-    else if((height/2<_posSourisY) && (player.getZ()<LARGEUR-this->_taille)){
-      player.setZ(player.getZ()+this->_vitesse*(cos(angle)));
-      z_cam=z_cam+this->_vitesse*(cos(angle));
+  float sourisCenterX = _posSourisX - width/2 ;
+  float sourisCenterY = _posSourisY - height/2;
+  float Magnitude = sqrt( pow( sourisCenterX , 2 ) + pow( sourisCenterY , 2 ) );
+  if( Magnitude != 0 ) {
+    //Vecteur unitaire de déplacement
+    float vX = _vitesse * sourisCenterX / Magnitude;
+    float vZ = _vitesse * sourisCenterY / Magnitude;
+
+    //position à la prochaine frame
+    float pX = this->getX() + vX;
+    float pZ = this->getZ() + vZ;
+
+    //si reste dans la bordure on bouge
+    if(
+      -LARGEUR + _taille <= pX && pX <= LARGEUR - _taille
+      &&
+      -LONGUEUR + _taille <= pZ && pZ <= LONGUEUR - _taille
+    ) {
+      this->setX( pX );
+      this->setZ( pZ );
+      x_cam += vX;
+      z_cam += vZ;
     }
   }
 }
+
