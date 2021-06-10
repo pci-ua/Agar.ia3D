6cb78623a1a64061b0480ff1c011a3d947f58cb3
StrategFirst <mathieu.toulon@free.fr>
2021-06-10 21:21:29 +0200

    Revu classse des participants

--- jeu_complet/joueurs.cpp
+++ jeu_complet/joueurs.cpp
@@ -1,26 +1,29 @@
 #include "joueurs.hh"
 
+#include <time.h>
+#include <stdlib.h>
+
 // augmente la taille du joueurs et diminue sa vitesse
 void Joueurs::mangerf(Food const & objet) {
-  this->_taille+=(MODIF_TAILLE/(this->_taille));
-  if(this->_vitesse>PALLIER_VITESSE)
-    this->_vitesse-=MODIF_VITESSE;
-  else if (this->_vitesse>VITESSE_MIN)
-    this->_vitesse=VITESSE_MIN;
+     this->_taille+=(MODIF_TAILLE/(this->_taille));
+     if(this->_vitesse>PALLIER_VITESSE)
+          this->_vitesse-=MODIF_VITESSE;
+     else if (this->_vitesse>VITESSE_MIN)
+          this->_vitesse=VITESSE_MIN;
 }
 
 // augmente la taille du joueurs et diminue sa vitesse en fonction de la taille du joueurs mangé
 void Joueurs::mangerj(Joueurs const & objet){
-  this->_taille+=((objet.getTaille()*POURCENTAGE_MANGE)/(this->_taille));
-  if(this->_vitesse>MODIF_VITESSE*objet.getTaille()*10)
-    this->_vitesse-=MODIF_VITESSE*objet.getTaille()*10;
-  else this->_vitesse=VITESSE_MIN;
+     this->_taille+=((objet.getTaille()*POURCENTAGE_MANGE)/(this->_taille));
+     if(this->_vitesse>MODIF_VITESSE*objet.getTaille()*10)
+          this->_vitesse-=MODIF_VITESSE*objet.getTaille()*10;
+     else this->_vitesse=VITESSE_MIN;
 }
 
 // réinitialise la taille, la vitesse du joueurs et change sa position
 void Joueurs::SeFaireManger(){
-  this->_posX=(float)rand()/(RAND_MAX)*(LONGUEUR-MARGE+LONGUEUR-MARGE)-LONGUEUR+MARGE;
-  this->_posZ=(float)rand()/(RAND_MAX)*(LARGEUR-MARGE+LARGEUR-MARGE)-LARGEUR+MARGE;
-  this->_taille=TAILLE_JOUEURS;
-  this->_vitesse=VITESSE;
+       this->_posX=(float)rand()/(RAND_MAX)*(LONGUEUR-MARGE+LONGUEUR-MARGE)-LONGUEUR+MARGE;
+       this->_posZ=(float)rand()/(RAND_MAX)*(LARGEUR-MARGE+LARGEUR-MARGE)-LARGEUR+MARGE;
+       this->_taille=TAILLE_JOUEURS;
+       this->_vitesse=VITESSE;
 }
--- jeu_complet/joueurs.hh
+++ jeu_complet/joueurs.hh
@@ -1,12 +1,4 @@
 #pragma once
-#include <iostream>
-#include <GL/glut.h>
-#include <GL/gl.h>
-#include <math.h>
-#include<GL/freeglut.h>
-#include <time.h>
-#include <stdlib.h>
-#include <GL/glu.h>
 
 #include "boule.hh"
 #include "food.hh"
@@ -14,12 +6,15 @@
 #define PI 3.14159265
 
 class Joueurs : public Boule{
-  public:
-    Joueurs():Boule(TAILLE_JOUEURS),_vitesse(VITESSE){}
-    ~Joueurs(){}
-    void mangerf(Food const & objet);
-    void mangerj(Joueurs const & objet);
-    void SeFaireManger();
+public:
+     Joueurs():Boule(TAILLE_JOUEURS),_vitesse(VITESSE){}
+     ~Joueurs() = default;
+     Joueurs(const Joueurs & c) = default;
+
+     void mangerf(Food const & objet);
+     void mangerj(Joueurs const & objet);
+     void SeFaireManger();
+     
   protected:
     float _vitesse;
 };
