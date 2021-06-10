a60d0870bd24005eab8ef0c9de5b4ebf675528ed
StrategFirst <mathieu.toulon@free.fr>
2021-06-10 19:51:32 +0200

    Revu de la classe mère des entitées

--- jeu_complet/boule.cpp
+++ jeu_complet/boule.cpp
@@ -1,10 +1,15 @@
 #include "boule.hh"
 
-Boule::~Boule(){
+// Pour le random
+#include <time.h>
+#include <stdlib.h>
 
-}
+// Pour le dessin
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include <GL/freeglut.h>
+#include <GL/glu.h>
 
-//dessine la boule à un endroit
 void Boule::draw() const{
  glPushMatrix();
  {
@@ -13,4 +18,15 @@ void Boule::draw() const{
    glutSolidSphere(static_cast<double>(_taille),SUBDIVISIONS_BOULE,SUBDIVISIONS_BOULE); // créer une sphère
  }
  glPopMatrix();
+}
+
+Boule::Boule(float taille) {
+  // rand et RAND_MAX viennent de la bibliothèque stdlib.h
+  // le calcul suivant correspond à prendre un nombre aléatoire (flottant) compris dans le terrain
+  this->_posX = (float)rand()/(RAND_MAX)*(LONGUEUR-MARGE+LONGUEUR-MARGE)-LONGUEUR+MARGE;
+  this->_posZ = (float)rand()/(RAND_MAX)*(LARGEUR-MARGE+LARGEUR-MARGE)-LARGEUR+MARGE;
+  this->_taille = taille;
+  this->_r = COULEUR; // nombre flottant aléatoire entre 0 et 1
+  this->_g = COULEUR;
+  this->_b = COULEUR;
 }
--- jeu_complet/boule.hh
+++ jeu_complet/boule.hh
@@ -1,33 +1,19 @@
 #pragma once
-#include <iostream>
-#include <GL/glut.h>
-#include <GL/gl.h>
-#include <math.h>
-#include<GL/freeglut.h>
-#include <time.h>
-#include <stdlib.h>
-#include <GL/glu.h>
 
 #include "constante.hh"
 
-// Super-classe de toutes les entitées
 class Boule{
   public:
-    Boule(float taille){
-      // rand et RAND_MAX viennent de la bibliothèque stdlib.h
-      // le calcul suivant correspond à prendre un nombre aléatoire (flottant) compris dans le terrain
-      this->_posX = (float)rand()/(RAND_MAX)*(LONGUEUR-MARGE+LONGUEUR-MARGE)-LONGUEUR+MARGE;
-      this->_posZ = (float)rand()/(RAND_MAX)*(LARGEUR-MARGE+LARGEUR-MARGE)-LARGEUR+MARGE;
-      this->_taille = taille;
-      this->_r = COULEUR; // nombre flottant aléatoire entre 0 et 1
-      this->_g = COULEUR;
-      this->_b = COULEUR;
-    }
-    ~Boule();
+    Boule(float taille);
+    ~Boule() = default;
+    Boule(const Boule & c) = default;
+
     void draw() const;
+
     float getX() const{ return _posX;}
     float getZ() const{ return _posZ;}
     float getTaille() const{return _taille;}
+
     void setX(float x){this->_posX=x;}
     void setZ(float z){this->_posZ=z;}
     void setr(float r){this->_r=r;}
@@ -38,6 +24,7 @@ class Boule{
     float _posX;
     float _posZ;
     float _taille;
+
     float _r;
     float _g;
     float _b;
