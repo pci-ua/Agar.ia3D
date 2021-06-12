5315e07e1c4b4eb81332110579fd40b226c92d06
StrategFirst <mathieu.toulon@free.fr>
2021-06-13 00:52:12 +0200

    IA fix

--- jeu_complet/ia.cpp
+++ jeu_complet/ia.cpp
@@ -7,6 +7,7 @@ void Ia::deplacement (int width,int height){
   // stock un nombre aléatoire entre 0 et la taille de la fenêtre
   int x2= (rand()%width) - width/2;
   int y2= (rand()%height) - height/2;
+
   // stocke les positions aléatoires dans des variables de la classe ia (_posAleatoireX,_posAleatoireY)
   this->setiaX(x2);
   this->setiaY(y2);
@@ -14,12 +15,12 @@ void Ia::deplacement (int width,int height){
   float Magnitude = sqrt( pow( _posAleatoireX , 2 ) + pow( _posAleatoireY , 2 ) );
   if( Magnitude != 0 ) {
     //Vecteur unitaire de déplacement
-    float vX = _vitesse * _posAleatoireX / Magnitude;
-    float vZ = _vitesse * _posAleatoireY / Magnitude;
+    float vX = _vitesse * ((float)(_posAleatoireX)) / Magnitude;
+    float vZ = _vitesse * ((float)(_posAleatoireY)) / Magnitude;
 
     //position à la prochaine frame
     float pX = _position.getX() + vX;
-    float pZ = _position.getX() + vZ;
+    float pZ = _position.getZ() + vZ;
 
     //si reste dans la bordure on bouge
     if(
