ef5371e74ba95c57cafd13944cf5a94f9830585b
StrategFirst <mathieu.toulon@free.fr>
2021-06-17 01:19:34 +0200

    Ajout conversion vers HSL et HSV

--- Modele/couleur/couleur.cc
+++ Modele/couleur/couleur.cc
@@ -1,6 +1,8 @@
 #include "couleur.hh"
 
 #include <iomanip>
+#include <algorithm>
+#include <cmath>
 
 Couleur::Couleur(Composant r,Composant g,Composant b)
   :_r(r),_g(g),_b(b) {}
@@ -31,5 +33,53 @@ std::string Couleur::toString_hex() {
   return stream.str();
 }
 
-std::string Couleur::toString_hsv() { return ""; }
-std::string Couleur::toString_hsl() { return ""; }
+std::string Couleur::toString_hsv() {
+  float rp = static_cast<float>(_r)/255;
+  float gp = static_cast<float>(_g)/255;
+  float bp = static_cast<float>(_b)/255;
+  float cmax = std::max(std::max(rp,gp),bp);
+  float cmin = std::min(std::min(rp,gp),bp);
+  float delta = cmax - cmin;
+
+  float H;
+  if(delta == 0) H = 0;
+  else {
+    if(cmax == rp) { H = ((gp-bp)/delta); while(H>6) { H-=6;} while(H<0) { H+=6; } } else
+    if(cmax == gp) H = ((bp-rp)/delta)+2; else
+    if(cmax == bp) H = ((rp-gp)/delta)+4;
+    H*= 60;
+  }
+
+  float S;
+  if(cmax == 0) S = 0;
+  else S = delta / cmax;
+
+  float V = cmax;
+
+  return "hsv(" +std::to_string(H)+ "°, " +std::to_string(H)+ "%, " +std::to_string(V)+ "%)";
+ }
+std::string Couleur::toString_hsl() {
+  float rp = static_cast<float>(_r)/255;
+  float gp = static_cast<float>(_g)/255;
+  float bp = static_cast<float>(_b)/255;
+  float cmax = std::max(std::max(rp,gp),bp);
+  float cmin = std::min(std::min(rp,gp),bp);
+  float delta = cmax - cmin;
+
+  float H;
+  if(delta == 0) H = 0;
+  else {
+    if(cmax == rp) { H = ((gp-bp)/delta); while(H>6) { H-=6;} while(H<0) { H+=6; } } else
+    if(cmax == gp) H = ((bp-rp)/delta)+2; else
+    if(cmax == bp) H = ((rp-gp)/delta)+4;
+    H*= 60;
+  }
+
+  float L = (cmax + cmin)/2;
+
+  float S;
+  if(delta == 0) S = 0;
+  else S = delta / (1-abs(2*L -1));
+
+  return "hsl(" +std::to_string(H)+ "°, " +std::to_string(S)+ "%, " +std::to_string(L)+ "%)";
+}
