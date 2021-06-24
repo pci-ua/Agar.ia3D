f7bdca4ecedd43debbe25e34d2dcf682822bf3ae
StrategFirst <mathieu.toulon@free.fr>
2021-06-24 23:04:46 +0200

    Rendu d'éssaie

--- Vue/windowRender.cc
+++ Vue/windowRender.cc
@@ -2,12 +2,34 @@
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/freeglut.h>
+#include "../constante.hh"
 
-GLvoid Modelisation() {
-
+void Modelisation() {
+	// Initialisation
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	//Draw
+
+
+	// Caméra
+	gluLookAt(0,0.6,30,0,0,0,0,1,0);
+
+	// Terrain
+	glBegin(GL_QUADS);
+	glColor3f(0.0,0.7,0.0);
+	glVertex3f(CARTE::LONGUEUR,0.0,CARTE::LARGEUR);
+	glVertex3f(-CARTE::LONGUEUR,0.0,CARTE::LARGEUR);
+	glVertex3f(-CARTE::LONGUEUR,0.0,-CARTE::LARGEUR);
+	glVertex3f(CARTE::LONGUEUR,0.0,-CARTE::LARGEUR);
+	glEnd();
+
+	// Sphere
+	glPushMatrix();
+	glTranslatef(4,0,-4); //se positionne sur le terrain
+	glColor3f(1,0,0);
+	glutSolidSphere(5,30,30); // créer une sphère
+	glPopMatrix();
+
+	// Finition
 	glutSwapBuffers();
 }
