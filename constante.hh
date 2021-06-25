f798043def0a937280ed69d4fabe219fcfc68b99
StrategFirst <mathieu.toulon@free.fr>
2021-06-25 21:45:44 +0200

    Fix some value

--- constante.hh
+++ constante.hh
@@ -4,10 +4,10 @@ namespace CARTE {
 }
 
 namespace NOURRITURE {
-  const double TAILLE = 0.1;
+  const double TAILLE = 0.02;
 };
 
 namespace JOUEUR {
-  const double TAILLE = 1;
-  const double VITESSE = 0.1;
+  const double TAILLE = 0.1;
+  const double VITESSE = 0.05;
 }
