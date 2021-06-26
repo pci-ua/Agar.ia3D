7e30cb5b30c129a6d64231715d7ff9fb79a4af60
StrategFirst <mathieu.toulon@free.fr>
2021-06-26 12:59:39 +0200

    Add some new stats

--- constante.hh
+++ constante.hh
@@ -1,13 +1,19 @@
 namespace CARTE {
-  const int LARGEUR = 50;
-  const int LONGUEUR = 50;
-}
+     // Taille :
+     const int LARGEUR = 50; // axe Z
+     const int LONGUEUR = 50; // axe X
+};
 
 namespace NOURRITURE {
-  const double TAILLE = 0.1;
+     // Default statistics :
+     const double TAILLE = 0.1;
 };
 
 namespace JOUEUR {
-  const double TAILLE = 1;
-  const double VITESSE = 1;
-}
+     // Default statistics :
+     const double TAILLE = 1;
+     const double VITESSE = 1;
+
+     // Range of view :
+     const double ROV = 4; // describe how far (in player selfsize scale) a player can grab info
+};
