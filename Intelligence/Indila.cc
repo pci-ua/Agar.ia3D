466274d6528d13ff3b3f35ed6655fc0c38853c98
StrategFirst <mathieu.toulon@free.fr>
2021-06-26 13:35:43 +0200

    Mise à jour des modèles d'IA déjà présent. Pour supporter la nouvelle fonctionnalité de vision sur le jeu

--- Intelligence/Indila.cc
+++ Intelligence/Indila.cc
@@ -5,7 +5,7 @@ Indila::Indila()
   // mais vous pouvez ajouter / modifier ce que vous voulez !
   :Joueur(Couleur(0,128,128)) {}
 
-double Indila::deplacement() {
+double Indila::deplacement(std::vector<InfoEntitee> joueurs,std::vector<InfoEntitee> nourritures) {
   // Ici l'idée est juste de faire tournée l'ia sur elle même
   // pour cela on donne juste un angle de direction qui s'incrémente
   i+=0.1;
--- Intelligence/Indila.hh
+++ Intelligence/Indila.hh
@@ -19,7 +19,7 @@ public:
   // Fonction à implémenter ! (Une implémentation par défaut est proposée ! )
   // Doit retourner l'angle vers lequel l'ia doit se diriger
   //  Angle en radian, 0 correspondant à la droite et tournant dans le sens anti-horaire
-  double deplacement() override;
+  double deplacement(std::vector<InfoEntitee> joueurs,std::vector<InfoEntitee> nourritures) override;
 
 
 // À partir d'ici vous êtes libres !
