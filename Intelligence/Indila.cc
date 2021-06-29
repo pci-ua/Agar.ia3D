ec93783543facceec5508a89f805519311bb1cfd
StrategFirst <mathieu.toulon@free.fr>
2021-06-29 14:43:06 +0200

    Correction indentation code

--- Intelligence/Indila.cc
+++ Intelligence/Indila.cc
@@ -1,13 +1,13 @@
 #include "Indila.hh"
 
 Indila::Indila()
-  // Seul la couleur est nécessaire par défaut,
-  // mais vous pouvez ajouter / modifier ce que vous voulez !
-  :Joueur(Couleur(0,128,128)) {}
+     // Seul la couleur est nécessaire par défaut,
+     // mais vous pouvez ajouter / modifier ce que vous voulez !
+     :Joueur(Couleur(0,128,128)) {}
 
 double Indila::deplacement(std::vector<InfoEntitee> joueurs,std::vector<InfoEntitee> nourritures) {
-  // Ici l'idée est juste de faire tournée l'ia sur elle même
-  // pour cela on donne juste un angle de direction qui s'incrémente
-  i+=0.1;
-  return i;
+     // Ici l'idée est juste de faire tournée l'ia sur elle même
+     // pour cela on donne juste un angle de direction qui s'incrémente
+     i+=0.1;
+     return i;
 }
--- Intelligence/Indila.hh
+++ Intelligence/Indila.hh
@@ -9,21 +9,21 @@ Tourner dans le vide, il me fait tourner
  ♫♫♫ */
 
 class Indila // N'hésitez pas à renommer votre IA !
-  :public Joueur { // Doit étendre Joueur pour pouvoir participer au jeu !
+     :public Joueur { // Doit étendre Joueur pour pouvoir participer au jeu !
 
 // Quelques éléments obligatoires ! :
 public:
-  // Constructeur, nécessaire ! (Pour transmettre des paramètres aux classes hérités ! )
-  Indila();
+     // Constructeur, nécessaire ! (Pour transmettre des paramètres aux classes hérités ! )
+     Indila();
 
-  // Fonction à implémenter ! (Une implémentation par défaut est proposée ! )
-  // Doit retourner l'angle vers lequel l'ia doit se diriger
-  //  Angle en radian, 0 correspondant à la droite et tournant dans le sens anti-horaire
-  double deplacement(std::vector<InfoEntitee> joueurs,std::vector<InfoEntitee> nourritures) override;
+     // Fonction à implémenter ! (Une implémentation par défaut est proposée ! )
+     // Doit retourner l'angle vers lequel l'ia doit se diriger
+     //  Angle en radian, 0 correspondant à la droite et tournant dans le sens anti-horaire
+     double deplacement(std::vector<InfoEntitee> joueurs,std::vector<InfoEntitee> nourritures) override;
 
 
 // À partir d'ici vous êtes libres !
 private:
-  double i = 0; // Sert seulement pour l'exemple d'implémentation de la fonction déplacement
+     double i = 0; // Sert seulement pour l'exemple d'implémentation de la fonction déplacement
 
 };
